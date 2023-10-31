b979457c75fd66e0325c2cfdbfada71a57d6260d
Diganta Si <diganta.si@gmail.com>
2023-10-31 20:27:38 +0530

    Started with basic structure of Neutral News

---
+++ Neutral-News/src/main/java/com/acuityspace/neutral/news/NeutralNewsApplication.java
@@ -0,0 +1,13 @@
+package com.acuityspace.neutral.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeutralNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeutralNewsApplication.class, args);
+	}
+
+}
---
+++ Neutral-News/src/test/java/com/acuityspace/neutral/news/NeutralNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acuityspace.neutral.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeutralNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
