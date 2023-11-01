ab44ae419fc3b2a5ec6d35859f9869c4e3a3fcdd
Diganta Si <diganta.si@gmail.com>
2023-11-01 09:45:36 +0530

    Added wildfly deployment config

---
+++ Neutral-News/src/main/java/com/acuityspace/neutral/news/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.acuityspace.neutral.news.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+    public DemoController() {
+    }
+
+    @GetMapping()
+    public String getString() {
+        return "Hello World";
+    }
+}
---
+++ Neutral-News/src/main/java/com/acuityspace/neutral/news/util/package-info.java
@@ -0,0 +1,1 @@
+package com.acuityspace.neutral.news.util;
