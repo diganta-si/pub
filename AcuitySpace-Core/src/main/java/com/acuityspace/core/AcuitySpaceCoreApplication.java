3236206587882fc214d896941d2e037d9aa6d897
Diganta Si <diganta.si@gmail.com>
2024-02-10 22:55:27 +0530

    Exposed actuator end-points

--- AcuitySpace-Core/src/main/java/com/acuityspace/core/AcuitySpaceCoreApplication.java
+++ AcuitySpace-Core/src/main/java/com/acuityspace/core/AcuitySpaceCoreApplication.java
@@ -1,9 +1,10 @@
 package com.acuityspace.core;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.jersey.JerseyServerMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JerseyServerMetricsAutoConfiguration.class)
 public class AcuitySpaceCoreApplication {
 
 	public static void main(String[] args) {
