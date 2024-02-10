3a7c2bbb55077f460b15fc11e6628e6933e37e5d
Diganta Si <diganta.si@gmail.com>
2024-02-10 23:33:45 +0530

    Changing to serve get or post both

--- Neutral-News/src/main/java/com/acuityspace/neutral/news/controller/TestingController.java
+++ Neutral-News/src/main/java/com/acuityspace/neutral/news/controller/TestingController.java
@@ -1,18 +1,21 @@
 package com.acuityspace.neutral.news.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestingController {
 
-	@PostMapping(path = "/test", consumes = { "text/plain", "application/*" }, produces = { "text/plain",
-			"application/*" })
-	public ResponseEntity<?> postMessage(@RequestBody String studentName) {
-		return new ResponseEntity<String>("S", HttpStatus.OK);
+	@RequestMapping(path = "/test", method = { RequestMethod.GET, RequestMethod.POST }, consumes = MediaType.ALL_VALUE, produces = MediaType.ALL_VALUE)
+	public ResponseEntity<?> testMessage(@RequestBody(required = false) String data) {
+		if (data != null) {
+			return new ResponseEntity<String>("Testing Successful with data = {" + data + "}", HttpStatus.OK);
+		}
+		return new ResponseEntity<String>("Testing Successful.", HttpStatus.OK);
 	}
-
 }
