7c0e5e841c5f821cad8419e2102bc752a5484499
Diganta Si <diganta.si@gmail.com>
2024-02-10 22:57:02 +0530

    Exposed actuator end-points

--- Neutral-News/src/main/java/com/acuityspace/neutral/news/NeutralNewsApplication.java
+++ Neutral-News/src/main/java/com/acuityspace/neutral/news/NeutralNewsApplication.java
@@ -1,13 +1,13 @@
 package com.acuityspace.neutral.news;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.jersey.JerseyServerMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = JerseyServerMetricsAutoConfiguration.class)
 public class NeutralNewsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NeutralNewsApplication.class, args);
 	}
-
 }
