ce01ae6743064676ab1548f8a5b453a830ecea37
Diganta Si <diganta.si@gmail.com>
2024-08-22 20:27:35 +0530

    Minor version modification

--- AcuitySpace-Core/src/main/java/com/acuityspace/core/AcuitySpaceCoreApplication.java
+++ AcuitySpace-Core/src/main/java/com/acuityspace/core/AcuitySpaceCoreApplication.java
@@ -3,7 +3,9 @@ package com.acuityspace.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.jersey.JerseyServerMetricsAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = JerseyServerMetricsAutoConfiguration.class)
 public class AcuitySpaceCoreApplication {
 
