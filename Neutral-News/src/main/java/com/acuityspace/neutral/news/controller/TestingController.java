35a6a9e2841b4d96ab4a4d282f8993ce73d8d62d
Diganta Si <diganta.si@gmail.com>
2024-08-22 20:25:05 +0530

    Minor version modification

--- Neutral-News/src/main/java/com/acuityspace/neutral/news/controller/TestingController.java
+++ Neutral-News/src/main/java/com/acuityspace/neutral/news/controller/TestingController.java
@@ -12,10 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestingController {
 
 	@RequestMapping(path = "/test", method = { RequestMethod.GET, RequestMethod.POST }, consumes = MediaType.ALL_VALUE, produces = MediaType.ALL_VALUE)
-	public ResponseEntity<?> testMessage(@RequestBody(required = false) String data) {
+	public ResponseEntity<Object> testMessage(@RequestBody(required = false) String data) {
 		if (data != null) {
-			return new ResponseEntity<String>("Testing Successful with data = {" + data + "}", HttpStatus.OK);
+			return new ResponseEntity<>("Testing Successful with data = {" + data + "}", HttpStatus.OK);
 		}
-		return new ResponseEntity<String>("Testing Successful.", HttpStatus.OK);
+		return new ResponseEntity<>("Testing Successful.", HttpStatus.OK);
 	}
 }
